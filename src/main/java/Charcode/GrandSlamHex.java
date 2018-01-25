713e6c7f667d00e31d307a0acdddf3a6a857a427
Martin Saitta <33335720+gigatron4624@users.noreply.github.com>
2018-01-25 11:52:58 -0600

    Update GrandSlamHex.java
    Changed variables from uppercase to lowercase (vice-versa for fields)

--- src/main/java/Charcode/GrandSlamHex.java
+++ src/main/java/Charcode/GrandSlamHex.java
@@ -9,28 +9,28 @@ import java.util.Base64;
 public class GrandSlamHex {
 
     /* Fields (Constants) */
-    private String sdi = "0123456789"; // sdi = single digit integers
-    private int a = 100;
+    private String SDI = "0123456789"; // SDI = single digit integers
+    private int A = 100;
 
     /* Package-Private Methods */
 
-    /** TA generates a byte array of a specific length whose elements are assigned values from 0 to x-1.
+    /** Method ta generates a byte array of a specific length whose elements are assigned values from 0 to x-1.
      * @param x is the number of elements in the byte array (i.e. byte array length);
      * @return a byte array that stores integers from 0 to x-1 in numeric order */
-    byte[] TA(int x){
-        byte[] X = new byte[x]; // initializes byte array of length x
+    byte[] ta(int x){
+        byte[] b = new byte[x]; // initializes byte array of length x
 
         for (byte i = 0; i < x; i++){
-            X[i] = i; // assign byte to each array element
+            b[i] = i; // assign byte to each array element
         }
-        return X;
+        return b; // return the byte array
     }
 
     /** Repeat concatenates a string to itself until it reaches a length of 50.
      * @param s is the string
      * @return the self-concatenated string
      */
-    String Repeat(String s){
+    String repeat(String s){
         StringBuilder root = new StringBuilder(s); // initialize a root string
 
         while (root.length() < 50){
@@ -47,81 +47,81 @@ public class GrandSlamHex {
     }
 
     /** b2d decodes an array of bytes into an array of decimal integers.
-     * @param B is the input byte array
+     * @param b is the input byte array
      * @return an array of decimal integers as a string */
-    String b2d(byte[] B){
-        return Arrays.toString(B); // returns a string representation of a decimal array
+    String b2d(byte[] b){
+        return Arrays.toString(b); // returns a string representation of a decimal array
     }
 
     /** b2h is similar to b2d, except that it decodes bytes to hexadecimal numbers.
-     * @param B is the input byte array
+     * @param b is the input byte array
      * @return an array of hexadecimal integers as a string */
-    String b2h(byte[] B){
-        String[] HA = new String[B.length]; // HA is the initial hex array as a string
+    String b2h(byte[] b){
+        String[] ha = new String[b.length]; // ha is the initial hex array as a string
 
-        for(int i = 0; i < B.length; i++){
-            HA[i] = String.format("%02x",B[i]); // converts each element to a 2-digit hexadecimal integer
+        for(int i = 0; i < b.length; i++){
+            ha[i] = String.format("%02x",b[i]); // converts each element to a 2-digit hexadecimal integer
         }
-        return Arrays.toString(HA); // returns a string representation of a hex array
+        return Arrays.toString(ha); // returns a string representation of a hex array
     }
 
-    /** Whomp encodes bytes into a string using the standard Base64 scheme.
+    /** whomp encodes bytes into a string using the standard Base64 scheme.
      * A whomp is an enemy in Super Mario 64 (1996).
-     * @param B is the input byte array
+     * @param b is the input byte array
      * @return the byte array encoded as a Base64 string */
-    String Whomp(byte[] B){
-        return Base64.getEncoder().encodeToString(B); // uses standard Base64 encoder
+    String whomp(byte[] b){
+        return Base64.getEncoder().encodeToString(b); // uses standard Base64 encoder
     }
 
-    /** Bomp encodes bytes into a string using the URL and filename safe Base64 scheme.
+    /** bomp encodes bytes into a string using the URL and filename safe Base64 scheme.
      * A bomp is also an enemy in Super Mario 64 (1996).
-     * @param B is the input byte array
+     * @param b is the input byte array
      * @return the byte array encoded as a Base64url string */
-    String Bomp(byte[] B){
-        return Base64.getUrlEncoder().encodeToString(B); // uses URL-safe Base64 encoder
+    String bomp(byte[] b){
+        return Base64.getUrlEncoder().encodeToString(b); // uses URL-safe Base64 encoder
     }
 
     /* Print to Console (private) */
 
     private void ptoc(){
-        String ccs = Repeat(sdi); // ccs = concatenated character string
+        String ccs = repeat(SDI); // ccs = concatenated character string
         System.out.println(ccs); // prints ccs
 
-        byte[] BA = TA(a); // BA = byte array (first input)
-        byte[] SDI = s2b(ccs); // SDI = single digit integers (second input)
+        byte[] ba = ta(A); // ba = byte array (first input)
+        byte[] csa = s2b(ccs); // csa = character string array (second input)
 
-        String ds1 = b2d(BA); // ds1 = 1st input as string of decimal array
+        String ds1 = b2d(ba); // ds1 = 1st input as string of decimal array
         System.out.println(ds1); // print ds1
-        String ds2 = b2d(SDI); // ds2 = 2nd input as string of decimal array
+        String ds2 = b2d(csa); // ds2 = 2nd input as string of decimal array
         System.out.println(ds2); // print ds2
 
-        String hs1 = b2h(BA); // hs1 = 1st input as string of hex array
+        String hs1 = b2h(ba); // hs1 = 1st input as string of hex array
         System.out.println(hs1); // print hs1
         System.out.println("Array Length = " + hs1.length()); // length of hs1 as full array
         System.out.println("Hex Length = " + hs1.length()/2); // length of hs1 without spaces, brackets, ot commas
 
-        String hs2 = b2h(SDI); // hs2 = 2nd array input as string of hex array
+        String hs2 = b2h(csa); // hs2 = 2nd array input as string of hex array
         System.out.println(hs2); // print hs2
         System.out.println("Array Length = " + hs2.length()); // length of hs2 as full array
         System.out.println("Hex Length = " + hs2.length()/2); // length of hs2 without spaces, brackets, or commas
 
         // whomp1 is the 1st input encoded as a Base64 string.
-        String whomp1 = Whomp(BA);
+        String whomp1 = whomp(ba);
         System.out.println(whomp1);
         System.out.println("Length = " + whomp1.length());
 
         // whomp2 is the 2nd input encoded as a Base64 string.
-        String whomp2 = Whomp(SDI);
+        String whomp2 = whomp(csa);
         System.out.println(whomp2);
         System.out.println("Length = " + whomp2.length());
 
         // bomp1 is the 1st input encoded as Base64url string.
-        String bomp1 = Bomp(BA);
+        String bomp1 = bomp(ba);
         System.out.println(bomp1);
         System.out.println("Length = " + bomp1.length());
 
         // bomp2 is the first input encoded as Base64url string.
-        String bomp2 = Bomp(SDI);
+        String bomp2 = bomp(csa);
         System.out.println(bomp2);
         System.out.println("Length = " + bomp2.length());
     }
