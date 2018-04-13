83eb601eb5a7bd8f2eb919be6f3f2ad5c7900cba
Martin Saitta <33335720+gigatron4624@users.noreply.github.com>
2018-04-12 19:01:48 -0500

    Now it works.
    I removed the address fields since they're already in the super-class.

--- src/test/java/Charcode/GearStuckOnNeutral.java
+++ src/test/java/Charcode/GearStuckOnNeutral.java
@@ -1,7 +1,7 @@
 /** This code uses TestNG assertion methods to check if TheGSONMenace functions properly. The purpose of assertion
  * testing in this case is to verify that both Address objects has identical content/fields. The first Address object
  * is initialized through standard procedure, while GSON converts the first instance to and from JSON to initialize
- * the second instance. */
+ * the second instance. GearStuckOnNeutral is a spoof on GSON. XD */
 
 package Charcode;
 
@@ -12,18 +12,19 @@ import org.testng.annotations.Test;
 
 public class GearStuckOnNeutral extends TheGSONMenace {
 
-    /* Address Fields */
-    private String tag = "WTF Productions"; // tag is another word for name; the address is fake
-    private String location = "7056 Blight Boulevard"; // street address; this one is fake
-    private String city = "East Bumf--k, Mississippi"; // Every address needs a city. This city is fake.
-    private int zone = 38809; // zone is the zip code (this one is fake)
-
     /** @return an error message should one or more assertion test fail */
     private String boo(){
         return "The gear stick is still jammed. Un-jam it now."; // assertion error message
     }
 
-    /** This method generates an Address instance with occupied fields. [1]
+    /* No Argument Constructor */
+    /* User beware: This is only placed for TestNG to run the tests. Otherwise use the constructor below. */
+    GearStuckOnNeutral(){
+        // Voila! Empty!
+    }
+
+    /* Address Instantiation Constructor */
+    /** This constructor generates an Address instance with occupied fields. [1]
      * @param tag      is the name of the place
      * @param location is the address of the place
      * @param city     is the city in which the address is located
@@ -38,14 +39,21 @@ public class GearStuckOnNeutral extends TheGSONMenace {
      * 3. Do the cities match?
      * 4. Do the zip codes match? */
 
-    @Test(alwaysRun = true)
+    @Test()
     void testGetters() throws Exception{
 
-        TheGSONMenace tgm1 = new TheGSONMenace(tag, location, city, zone); // initialize 1st Address instance
-        Gson shift = new GsonBuilder().create(); // builds a GSON instance
-        String mask = shift.toJson(tgm1); // converts address object to JSON string
-        TheGSONMenace tgm2 = shift.fromJson(mask, TheGSONMenace.class); // create 2nd address instance
+        String tag1 = "WTF Productions"; // This name is fake.
+        String location1 = "7056 Blight Boulevard"; // This street address is fake.
+        String city1 = "East Bumf--k, Mississippi"; // EThis city is fake.
+        int zone1 = 38809; // This zip code is fake.
 
+        // initialize 1st Address instance
+        GearStuckOnNeutral tgm1 = new GearStuckOnNeutral(tag1, location1, city1, zone1);
+        Gson shift = new GsonBuilder().create(); // builds a GSON instance (shift variables to JSON object)
+        String json = shift.toJson(tgm1); // converts address object to JSON string
+        GearStuckOnNeutral tgm2 = shift.fromJson(json, GearStuckOnNeutral.class); // create 2nd address instance
+
+        //The TestNG assertions test begins here.
         Assert.assertEquals(tgm1.getTag(),tgm2.getTag(),boo()); // Do the names match?
         Assert.assertEquals(tgm1.getLocation(),tgm2.getLocation(),boo()); // Do the locations match?
         Assert.assertEquals(tgm1.getCity(),tgm2.getCity(),boo()); // Do the cities match?
